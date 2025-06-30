7ce1887e201585d59e3ad2c3a0d996f54c367b3d
MinhNN <minhnguyennhatt@gmail.com>
2025-06-30 14:45:43 +0700

    Session07

--- PTIT_CNTT1_IT201_Session07/PTIT_CNTT1_IT201_Session07_Bai06.c
+++ PTIT_CNTT1_IT201_Session07/PTIT_CNTT1_IT201_Session07_Bai06.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
 
+void merge(int arr[], int left, int mid, int right) {
+    int nLeft = mid - left + 1;
+    int nRight = right - mid;
+
+    int L[nLeft], R[nRight];
+
+    for (int i = 0; i < nLeft; i++) {
+        L[i] = arr[left + i];
+    }
+    for (int i = 0; i < nRight; i++){
+        R[i] = arr[mid + 1 + i];
+    }
+
+    int i = 0, j = 0, k = left;
+    while (i < nLeft && j < nRight) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while (i < nLeft) {
+        arr[k++] = L[i++];
+    }
+    while (j < nRight) {
+        arr[k++] = R[j++];
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
 int main() {
     int n;
     printf("Nhap so luong phan tu: ");
@@ -23,7 +63,12 @@ int main() {
 
     printf("\n");
 
+    mergeSort(arr, 0, n-1);
 
+    printf("after: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
