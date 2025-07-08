9d3c6a1564ef35637fc4cb1453af4c205499abc2
MinhNN <minhnguyennhatt@gmail.com>
2025-07-09 00:21:10 +0700

    Session13

--- PTIT_CNTT1_IT201_Session13/PTIT_CNTT1_IT201_Session13_Bai07.c
+++ PTIT_CNTT1_IT201_Session13/PTIT_CNTT1_IT201_Session13_Bai07.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct Stack {
     char* data;
@@ -8,36 +9,67 @@ typedef struct Stack {
     int cap;
 } Stack;
 
-Stack createSteak(int size) {
-    Stack temp;
-    temp.data = (char*)malloc(size*sizeof(char));
-    temp.top = -1;
-    temp.cap = size;
-    return temp;
+Stack createStack(int size) {
+    Stack s;
+    s.data = (char*)malloc(size * sizeof(char));
+    s.top = -1;
+    s.cap = size;
+    return s;
 }
 
-void push(Stack* stack, int value) {
-    if (stack->top >= stack->cap - 1) {
-        printf("Stack is full\n");
-        return;
+void push(Stack* s, char c) {
+    if (s->top < s->cap - 1) {
+        s->data[++(s->top)] = c;
     }
-    stack->top++;
-    stack->data[stack->top] = value;
 }
 
-int pop(Stack* stack) {
-    if (stack -> top == -1) {
-        printf("No element in stack");
-        return -1;
+char pop(Stack* s) {
+    if (s->top == -1) return '\0';
+    return s->data[(s->top)--];
+}
+
+char peek(Stack* s) {
+    if (s->top == -1) return '\0';
+    return s->data[s->top];
+}
+
+bool isMatching(char open, char close) {
+    return (open == '(' && close == ')') ||
+           (open == '[' && close == ']') ||
+           (open == '{' && close == '}');
+}
+
+bool isValidExpression(const char* expr) {
+    Stack s = createStack(strlen(expr));
+    for (int i = 0; expr[i] != '\0'; i++) {
+        char c = expr[i];
+        if (c == '(' || c == '[' || c == '{') {
+            push(&s, c);
+        } else if (c == ')' || c == ']' || c == '}') {
+            char top = peek(&s);
+            if (s.top == -1 || !isMatching(top, c)) {
+                free(s.data);
+                return false;
+            }
+            pop(&s);
+        }
     }
-    int temp = stack -> data[stack->top];
-    stack->top--;
-    return temp;
+    bool result = (s.top == -1);
+    free(s.data);
+    return result;
 }
-
-void
 
 int main() {
+    char expr[200];
+    printf("Nhap bieu thuc: ");
+    fgets(expr, sizeof(expr), stdin);
+    expr[strlen(expr) - 1] = '\0';
+
+    if (isValidExpression(expr)) {
+        printf("Bieu thu hop le\n");
+    } else {
+        printf("Bieu thuc khong hop le\n");
+    }
 
     return 0;
 }
